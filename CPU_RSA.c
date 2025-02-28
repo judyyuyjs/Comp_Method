34bf4cc9d6065109b95044fcb2c4d4c5eda2ca09
ya-tung63 <114728239+ya-tung63@users.noreply.github.com>
2025-02-28 14:59:51 -0500

    Update CPU_RSA.c

--- CPU_RSA.c
+++ CPU_RSA.c
@@ -3,27 +3,30 @@
 #include <math.h>
 // #include <bits/stdc++.h>
 // using namespace std;
-// LP problem data                 standard form
-// min z = 2x1 + 3x2               min Z = 2x1 + 3x2 
-// s.t. x1 + 2x2 <= 4              s.t. x1 + 2x2 + s1 = 4
-//      x1 + x2  <= 3                   x1 + x2 + s2 = 3
-//      x1, x2 >= 0                   
+// LP problem data                        standard form
+// min z = -60x1 - 30x2 - 20x3            min Z = -60x1 - 30x2 - 20x3
+// s.t. 8x1 + 6x2 + x3 <= 48              s.t. 8x1 + 6x2 + x3 + s1 = 48
+//      4x1 + 2x2 + 1.5x3 <= 20                4x1 + 2x2 + 1.5x3 + s2 = 20
+//      2x1 + 1.5x2 + 0.5x3 <= 8               2x1 + 1.5x2 + 0.5x3 + s3 = 8
+//      x1, x2, x3 >= 0                   x1, x2, x3, s1, s2, s3 >= 0
 
 // data is LP problem in standard form
 // min z = cx
 // s.t. Ax = b
 //      x >= 0
-#define N 2
-#define M 2
-#define num_row 2
-#define num_col 4
+// input the data here
+#define num_row 3
+#define num_col 6
+#define N 3  // number of basic variable
+#define M 3  // number of non basic variable
 
-double A[2][4] = {
-    {2, 1, 1, 0},
-    {1, 2, 0, 1}
+double A[num_row][num_col] = {
+    {8, 6, 1, 1, 0, 0},
+    {4, 2, 1.5, 0, 1, 0},
+    {2, 1.5, 0.5, 0, 0, 1}
 };
-double b[2] = {3, 3};
-double c[4] = {-1, -4, 0, 0};
+double b[num_row] = {48, 20, 8};
+double c[num_col] = {-60, -30, -20, 0,0,0};
 
 
 // calculate cofactor
@@ -104,12 +107,15 @@ void find_AJ(double A[num_row][num_col], int basis[], double Aj[N][N]) {
 int findMin(double arr[], int n) {
     int min = arr[0];
     int min_index = 0;
-    for (int i = 1; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         if (arr[i] < 0){
+            if (arr[i] == min){
+                min = arr[i+1];
+            }
             continue;
         }
 
-        if (arr[i] < min) {
+        if (arr[i] <= min) {
             min = arr[i];
             min_index = i;
         }
@@ -136,20 +142,12 @@ int assign_variable(int basic_index[N], int non_basic_index[M], int entering_var
 
 
 int main(){
-
-    // int num_row = sizeof(A) / sizeof(A[0]);
-    // int num_col = sizeof(A[0]) / sizeof(A[0][0]);
-
-    //int N = 2; // number of basic variable
-    //int M = num_col - N; // number of non basic variable
-
-    int status = 0;
-    // initial basis varible index
-    int basic_index[N] = {2, 3};
-    int non_basic_index[M] = {0, 1};
+    // input initial basis varible index here    
+    int basic_index[N] = {3, 4, 5};
+    int non_basic_index[M] = {0, 1, 2};
 
     int iter_num = 0;
-    while (iter_num <= 100) {
+    while (iter_num <= 10) {
         iter_num++;
         double Ab[N][N];
         find_AJ(A, basic_index, Ab);
@@ -196,13 +194,14 @@ int main(){
 
 
         // check if find CN_bar < 0 and find entering variable
+        int status = 0;
         int entering_index = 0;
         for (int i = 0; i < M; i++) {
             if (cN_bar[i] < 0) {
                 entering_index = i; // (use Bland's rule)
                 break;
             }
-            status = 1;
+            status += 1;
         }
 
         int entering_var = non_basic_index[entering_index];
@@ -216,8 +215,9 @@ int main(){
             }
 
         // if find optimal
-        if (status == 1) {
+        if (status == M) {
 
+            printf("Xb\n");
             for (int j = 0; j < N; j++) {
                 printf("%.3f%s", Xb[j], j == N - 1 ? "" : " ");
             }
@@ -227,7 +227,7 @@ int main(){
             for (int i = 0; i < N; i++){
                 z += p[i]*b[i];
             }
-            z = -1*z;
+            printf("optimal value\n");
             printf("%f", z);
 
             break; // find optimal! end loop
@@ -238,11 +238,11 @@ int main(){
                 colA[i] = A[i][entering_var];
             }
             
-        double Al[num_row];
-            for (int i = 0; i < N; i++) {
-                Al[i] = 0;
-                for (int j = 0; j < N; j++) {
-                    Al[i] += Ab_inverse[i][j] * colA[i];
+        double A_bar[num_row];
+            for (int i = 0; i < num_row; i++) {
+                A_bar[i] = 0;
+                for (int j = 0; j < num_row; j++) {
+                    A_bar[i] += Ab_inverse[i][j] * colA[j];
                 }
             }
 
@@ -251,8 +251,8 @@ int main(){
         int unboundedness = 0;
         
         for (int i = 0; i < num_row; i++) {
-            if (Al[i] > 0){
-                min_ratio[i] = Xb[i]/Al[i];
+            if ( A_bar[i] > 0){
+                min_ratio[i] = Xb[i]/ A_bar[i];
             } else {
                 min_ratio[i] = -1.0;
                 unboundedness += 1;
@@ -266,11 +266,9 @@ int main(){
         int leaving_index = 0;
         leaving_index = findMin(min_ratio, num_row);
         int leaving_var = basic_index[leaving_index];
-
+        
+        // new basic index
         assign_variable(basic_index, non_basic_index, entering_var, leaving_var);
-
-        // new basis Ab
-        printf("\n");
     }
 
 
